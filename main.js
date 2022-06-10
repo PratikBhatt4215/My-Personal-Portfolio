a5d6d3fff9ed025341a7a6dd5517d4dbcc93d93e
PratikBhatt <pk4789218@gmail.com>
2022-06-10 15:31:09 +0530

    Updating 14

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
 function scrollTop() {
     const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    // if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
     if (this.scrollY >= 560) {
         scrollTop.classList.add('show-scroll');
     } else {
@@ -23,13 +21,13 @@ function loader() {
 }
 
 function fadeOut() {
-    setTimeout(loader, 4000);
+    setTimeout(loader, 5000);
 }
 
-// var message = "You Cannot see my codes";
+var message = "You Cannot see my codes";
         
-// function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
 
-// if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
 
-// document.onmousedown = rtclickcheck;
+document.onmousedown = rtclickcheck;
